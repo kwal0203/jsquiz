78a1b4baf03db2124835374da98a63506bc4ee08
Kane Walter <kwal0203@gmail.com>
2014-01-19 18:22:39 +1100

    initial commit

---
+++ jsquiz.js
@@ -0,0 +1,92 @@
+var questions = [{question: "Whos the best bball player?", choices: ["Lebron James", "Kobe Bryant", "Kevin Durant", "Javale McGee"], correctAnswer:0},
+{question: "Name the Miami Heat player?", choices: ["Camelo Anthony", "Kevin Love", "Paul Pierce", "Dwayne Wade"], correctAnswer:3},
+{question: "Name the LA Lakers player?", choices: ["Lebron James", "Kobe Bryant", "Kevin Durant", "Javale McGee"], correctAnswer:1},
+{question: "What team does Melo play for?", choices: ["Chicago Bulls", "Utah Jazz", "New York Knicks", "Orlando Magic"], correctAnswer:2}
+];
+
+var score = 0;
+var questionNumber = 0;
+var button = document.getElementById('next');
+var backButton = document.getElementById('back');
+
+// set question and answers
+
+function setHeader(questionNumber){
+	var question = document.getElementById('question');
+	question.innerHTML = questions[questionNumber].question;
+}
+
+function setAnswer(elemId,questionNum,choiceNum){
+	var elem = document.getElementById(elemId);
+	elem.innerHTML = questions[questionNum].choices[choiceNum]
+}
+
+function askQuestion(){
+	setHeader(questionNumber);
+	setAnswer("choice_1",questionNumber,0);
+	setAnswer("choice_2",questionNumber,1);
+	setAnswer("choice_3",questionNumber,2);
+	setAnswer("choice_4",questionNumber,3);
+}
+
+// Adds up number of questions answered correctly
+
+function updateScore(){
+	var rightAnswer = questions[questionNumber].correctAnswer;
+	var chosenAnswer = document.getElementById("answ" + (rightAnswer + 1));
+	if (chosenAnswer.checked){
+		score++;
+	}
+}
+
+// displays next question and set of choices when the 'next' button is clicked
+
+function nextQuestion(){
+	var validates = validateForm();
+	if(validates){
+		updateScore();
+		questionNumber++;
+		showScore();
+		askQuestion();
+	}			
+}
+
+// sends user back to first question when 'back' button is clicked
+
+function questionreview(){
+	questionNumber = 0;
+	score = 0;
+	askQuestion();
+}
+
+// displays number of questions answered correctly at end of quiz
+
+function showScore(){
+	if(questionNumber == questions.length){
+		document.body.innerHTML = "Your score is " + score;
+	}
+}
+
+// ensures a user has answered the current question before moving to the next one
+
+function validateForm(){
+	var form = document.getElementsByName('choice');
+	var formValid = false;
+	var i = 0;
+
+	while(!formValid && i < questions[questionNumber].choices.length){
+		if(form[i].checked){
+			formValid = true;
+		}
+		i++;
+	}
+
+	if(!formValid){
+		alert("Please select an answer");
+	}
+	return formValid;
+}		
+
+addEventListener('load',askQuestion);
+button.addEventListener('click',nextQuestion,false);
+backButton.addEventListener('click',questionreview,false);
